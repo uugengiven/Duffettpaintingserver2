28a4e907f9b48200646e0844698c0d3b285c21af
bsduff3tt <97562569+bsduff3tt@users.noreply.github.com>
2024-08-08 16:24:45 -0400

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
+//import pg from "pg";
 import env from "dotenv";
 
 const app = express();
@@ -10,28 +10,28 @@ app.use(bodyParser.json());
 env.config();
 const PORT = process.env.PORT || 3000;
 
-const db = new pg.Client({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT
-  });
+//const db = new pg.Client({
+//    user: process.env.PG_USER,
+//    host: process.env.PG_HOST,
+//    database: process.env.PG_DATABASE,
+//    password: process.env.PG_PASSWORD,
+//    port: process.env.PG_PORT
+//  });
 
-db.connect();
+//db.connect();
 
-async function updateDatabase(name, contact, address){
-  const datetime = new Date();
-  await db.query(
-    "INSERT INTO Estimates (name, contact, address, estimatetime) VALUES ($1, $2, $3, $4)",
-    [name, contact, address, datetime]
-  );
-  try{
-    console.log("insert success");
-  }catch(err) {
-      console.log(err);
-  };
-}
+//async function updateDatabase(name, contact, address){
+//  const datetime = new Date();
+//  await db.query(
+//    "INSERT INTO Estimates (name, contact, address, estimatetime) VALUES ($1, $2, $3, $4)",
+//    [name, contact, address, datetime]
+//  );
+//  try{
+//    console.log("insert success");
+//  }catch(err) {
+//      console.log(err);
+//  };
+//}
 
 app.post("/", (req, res) => {
     console.log("here");
